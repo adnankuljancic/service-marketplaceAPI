2630d9ff27bf9a80d809a543fbbc74f97881b324
Adnan Kuljancic <adnan.kuljancic@outlook.com>
2023-11-11 11:50:45 +0100

    First controller

---
+++ ServiceMarketplaceAPI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceMarketplaceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("User user");
+        }
+    }
+}
