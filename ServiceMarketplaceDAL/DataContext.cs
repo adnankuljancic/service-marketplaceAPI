11548c6e5ab13f38e538665d03cde744542439a1
Adnan Kuljancic <adnan.kuljancic@outlook.com>
2023-11-17 21:25:17 +0100

    testing

--- ServiceMarketplaceDAL/DataContext.cs
+++ ServiceMarketplaceDAL/DataContext.cs
@@ -10,15 +10,9 @@ namespace ServiceMarketplaceDAL
 {
     public class DataContext : DbContext
     {
+        public DataContext() { }
         public DataContext(DbContextOptions options) : base(options) { }
         public DbSet<User> Users { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
-
-        }
     }
 }
-
