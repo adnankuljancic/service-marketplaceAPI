d285ffbf6d2dcb0ab7a6a986d090350e21dd70e7
Adnan Kuljancic <adnan.kuljancic@outlook.com>
2023-12-15 17:50:33 +0100

    Entitiy

---
+++ ServiceMarketplaceDAL/Entities/Review.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMarketplaceDAL.Entities
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ReviewText { get; set; } = "";
+        public int Grade { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set;}
+        public User User {  get; set; }
+
+        [ForeignKey("Service")]
+        public int ServiceId {  get; set; }
+        public Service Service { get; set; }
+
+
+    }
+}
