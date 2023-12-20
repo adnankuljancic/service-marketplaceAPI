9571541b5cdbe704e9965e85e6e3fd142d829495
Adnan Kuljancic <adnan.kuljancic@outlook.com>
2023-12-20 11:39:40 +0100

    Reviews done

--- ServiceMarketplaceDAL/Repository/ReviewRepository.cs
+++ ServiceMarketplaceDAL/Repository/ReviewRepository.cs
@@ -10,9 +10,18 @@ namespace ServiceMarketplaceDAL.Repository
 {
     public class ReviewRepository : IReviewRepository
     {
+        private readonly DataContext _dataContext;
+
+        public ReviewRepository (DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
         public async Task<Review> AddReview(Review review)
         {
-            throw new NotImplementedException();
+            await _dataContext.Reviews.AddAsync(review);
+            await _dataContext.SaveChangesAsync();
+            return review;
+
         }
     }
 }
